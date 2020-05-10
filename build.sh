9c7b8ed86b4421d1c25e81f6de62bcd08b015954
Sahal Zain <zean00zone@gmail.com>
2020-05-10 19:25:13 +0700

    enable dylib for linux-musl

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cargo build --release --target aarch64-linux-android
 cargo build --release --target armv7-linux-androideabi
 cargo build --release --target i686-linux-android
 cargo build --release --target x86_64-unknown-linux-gnu
-env CROSS_COMPILE=x86_64-linux-musl cargo build --release --target x86_64-unknown-linux-musl
+env RUSTFLAGS="-C target-feature=-crt-static" CROSS_COMPILE=x86_64-linux-musl cargo build --release --target x86_64-unknown-linux-musl
